aaa0b8771f79c1b657a0a14c58c64d9cebadcf4d
Illya Novohatskyy <1llyanovohatskyy@gmail.com>
2017-10-02 13:36:57 +0300

    fixed new line

--- pure.js
+++ pure.js
@@ -6,5 +6,4 @@ const pureFunc1 = x => Object.assign({}, x, { val: x.val + 1});
 
 const pureFunc2 = x => Object.assign({}, x, { val: x.val * 2});
 
-//use console.log() below
-pureFunc1(pureFunc2(obj)).val // 5
+console.log(pureFunc1(pureFunc2(obj)).val); // 5
