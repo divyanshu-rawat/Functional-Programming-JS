fa716583bebcf734231b3fd17328bcbc1a4795ef
yussan <yussandeveloper@gmail.com>
2017-10-02 11:07:11 +0700

    Update sort.js

--- sort.js
+++ sort.js
@@ -9,12 +9,8 @@ const animals = [
 const sortres = animals.sort((a, b) => {
 const nameA = a.name.toLowerCase();//ignore uper/lowercase
 const nameB = b.name.toLowerCase();//ignore uper/lowercase
-if (nameA < nameB) {
-  return -1;
-}
-if (nameA > nameB) {
-  return 1;
-}
+if (nameA < nameB) return -1;
+if (nameA > nameB) return 1;
 // names must be equal
 return 0;
 })
