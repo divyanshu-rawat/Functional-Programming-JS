fb5de20f96d3c06a04449f32e9daf6e36420f81d
black-hat-cross <pcross@prkit.la>
2017-10-06 12:38:34 -0700

    Added a needed space for closure example

--- closure.js
+++ closure.js
@@ -1,7 +1,7 @@
 function initialize() {
 	var name = 'Functional Programming in JS'; //local variable to initialize
 	function displaySummary() { //displaySummary() is the closure
-		console.log('The name of the project is' + name); //variable declared in the parent function is used 
+		console.log('The name of the project is ' + name); //variable declared in the parent function is used 
 	}
 	displaySummary();
 }
