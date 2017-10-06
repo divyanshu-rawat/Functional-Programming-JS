3b9de8d075d09c42c6f848069f982720bfcf5ab3
Jesse George Singleton <JesseSingleton@users.noreply.github.com>
2017-10-06 15:30:46 +0100

    Refactored reverse.js
    Changed:
    * Added Description
    * Added var to be more consitent

--- reverse.js
+++ reverse.js
@@ -1,12 +1,13 @@
+// The reverse() method reverses an array in place. The first array element becomes the last, and the last array element becomes the first.
+// REF: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+
 const animals = [
-    { name: 'Waffles', type: 'dog', age: 12 },
-    { name: 'Fluffy', type: 'cat', age: 14 },
-    { name: 'Spelunky', type: 'dog', age: 4 },
-    { name: 'Hank', type: 'dog', age: 11 } 
-  ];
+  { name: 'Waffles', type: 'dog', age: 12 },
+  { name: 'Fluffy', type: 'cat', age: 14 },
+  { name: 'Spelunky', type: 'dog', age: 4 },
+  { name: 'Hank', type: 'dog', age: 11 } 
+];
  
- /**
-  * result must be
-  * 1)Hank 2)Spelunky 3)Fluffy 4) Waffles
-  */ 
- console.log(animals.reverse())
+const reverse = animals.reverse();
+
+console.log(reverse);
