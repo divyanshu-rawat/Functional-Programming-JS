0e1af4a38186d9d075089766b7bfec996ba851cf
Jesse George Singleton <JesseSingleton@users.noreply.github.com>
2017-10-06 15:50:38 +0100

    Refactored recursion.js
    This was really difficult to read. Readibility > less lines of code especially when this intended for people to look up as reference. Also consitency is key, try not to use If statements AND ternery operators. Use either or.
    
    Changed:
    * Added comments
    * Refactored ternery operators into ifs to make it easier to read

--- recursion.js
+++ recursion.js
@@ -1,21 +1,34 @@
+// Return a list consisting of only the unique elements in this list
 
-/*
-	return a list consisting of only the unique elements in this list
-*/
+// Function to get random int using Math.Random() and Math.Floor()
+const getRandomInt = (min, max) => {
+	const random =  Math.floor(Math.random() * (max - min + 1));
+	return random + min;
+};
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+// Function to get random number between 1 and 100
 const getRandom1to100 = () => getRandomInt(1, 100);
 
+// Function to ad a unique integer
 const addUnique = ar => {
-    const el = getRandom1to100();
-    return ar.includes(el) ? ar : ar.concat([el]);
-}
+	const el = getRandom1to100();
+
+	if(ar.includes(el))
+		return ar;
+
+	return ar.concat([el]);
+};
 
 const uniqueArray = (numberOfElements, baseArray) => {
-    if (numberOfElements < baseArray.length) throw 'invalid input'; 
-    return baseArray.length === numberOfElements ? baseArray : 
-    		uniqueArray(numberOfElements, addUnique(baseArray));
-}
+	if (numberOfElements < baseArray.length) 
+			throw 'invalid input';
+
+	if(numberOfElements)
+		return baseArray.length === baseArray;
+	
+	return baseArray.length === uniqueArray(numberOfElements, addUnique(baseArray));
+};
 
 const myArray = uniqueArray(100, []);
+
 console.log(myArray);
